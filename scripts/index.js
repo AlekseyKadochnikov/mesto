5589e6b0f60edf41fa3bb0c10dc702b99b9d855f
Aleksey Kadochnikov <2002-466@bk.ru>
2021-03-28 16:10:31 +0500

    Первая попытка

---
+++ scripts/index.js
@@ -0,0 +1,50 @@
+let popup = document.querySelector('.popup');
+let content = document.querySelector('.content')
+let openPopupBtn = content.querySelector('.form__edit-btn');
+let closePopupBtn = popup.querySelector('.form__close-btn');
+let savePopupBtn = popup.querySelector('.form__submit-btn_action_save');
+let popupOverlay = popup.querySelector('.popup__overlay');
+
+function openPopup() {
+    popup.classList.add('popup__visible');
+}
+
+function closePopup() {
+    popup.classList.remove('popup__visible');
+}
+
+openPopupBtn.addEventListener('click', function() {
+    openPopup();
+});
+
+
+closePopupBtn.addEventListener('click', function (){
+    closePopup();
+});
+popupOverlay.addEventListener('click', function () {
+    closePopup();
+});
+
+savePopupBtn.addEventListener('click', function () {
+    closePopup();
+});
+
+document.querySelector('.popup').addEventListener('keyup', function (event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.querySelector(".form__submit-btn_action_save").click();
+        }
+});
+
+function addProfile() {
+    let name = document.querySelector('.input__text_type_name');
+    let job = document.querySelector('.input__text_type_job');
+
+    let profileName = document.querySelector('.profile-info__text_type_name');
+    let profileJob = document.querySelector('.profile-info__text_type_job');
+
+profileName.textContent = name.value;
+profileJob.textContent = job.value;
+}
+
+savePopupBtn.addEventListener('click', addProfile);
